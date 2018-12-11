05b7359f12166bf1ee88a77a75df196f00d4ae21
Max <max@neuvians.net>
2018-12-11 15:13:28 -0500

    moved dir

--- __tests__/lib/dynamo.test.js
+++ __tests__/lib/dynamo.test.js
@@ -1,13 +1,13 @@
 // import request from "supertest";
-import { loadFromDynamo, saveFromDynamo } from "../../lib/dynamo";
+import { loadFromDynamo, saveFromDynamo } from "../../src/lib/dynamo";
 
 let nock = require("nock");
 
 describe("loadFromDynamo", () => {
   it("returns a map of data related to a repo", async () => {
-    let db = nock("https://dynamodb.ca-central-1.amazonaws.com/")
+    nock("https://dynamodb.ca-central-1.amazonaws.com/")
       .post("/")
       .reply(200, { Items: {} });
-    let result = await loadFromDynamo("cds-snc/sample");
+    await loadFromDynamo("cds-snc/sample");
   });
 });
