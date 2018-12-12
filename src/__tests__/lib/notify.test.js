c8e5343b9a123afc5ede3575d79045ff51a16247
Tim Arney <tim@line37.com>
2018-12-12 13:52:21 -0500

    notify-tests

--- src/__tests__/lib/notify.test.js
+++ src/__tests__/lib/notify.test.js
@@ -1,14 +1,15 @@
 import { notify } from "../../lib/notify";
 import { webhook } from "../../__mocks__/webhook";
+import { createStatus } from "../../__mocks__/create-status";
 
 const octokit = {};
+const mockCallback = jest.fn(() => createStatus);
+
 octokit.authenticate = () => {
   return true;
 };
 octokit.repos = {};
-octokit.repos.createStatus = () => {
-  return true;
-};
+octokit.repos.createStatus = mockCallback;
 
 describe("handles notify", () => {
   it("returns false when no data is passed", async () => {
@@ -19,6 +20,7 @@ describe("handles notify", () => {
   it("returns true when passing valid data", async () => {
     const body = JSON.parse(webhook.body);
     let results = await notify(body, octokit);
-    expect(results).toEqual(true);
+    expect(results.data.hasOwnProperty("created_at")).toEqual(true);
+    expect(mockCallback.mock.calls.length).toBe(1);
   });
 });
--- src/handler.js
+++ src/handler.js
@@ -1,8 +1,6 @@
 "use strict";
 import { validate } from "./lib/validate";
 import { notify } from "./lib/notify";
-
-require("dotenv-safe").config({ allowEmptyValues: true });
 const octokit = require("@octokit/rest")();
 
 export const hello = async (event, context) => {
@@ -21,14 +19,14 @@ export const hello = async (event, context) => {
   // Step 4: Check out code from Github
   // Step 5: Validate webpack has the required plugins
   // Step 6: Run npm build
-  // Step 7: Calculate delta 
+  // Step 7: Calculate delta
   // Step 8. Post back to PR in Github
   // Step 9: Save new result set to DynamoDB
 
   // Potential Data structure
   // {repo: "", sha: "", branch: "", timestamp: "", data: {}}
 
-  console.log("This is the event:", event);
+  // console.log("This is the event:", event);
   return {
     statusCode: 200,
     body: JSON.stringify({
