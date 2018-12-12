73efe41fb448c80565af76ca907c0fb92e4ea3cb
Max <max@neuvians.net>
2018-12-12 15:16:09 -0500

    Small fix

--- src/lib/checkout.js
+++ src/lib/checkout.js
@@ -1,10 +1,14 @@
 const { spawnSync } = require("child_process");
 
-module.exports.checkoutRepo = async name => {
+module.exports.checkoutRepo = async fullName => {
   await require("lambda-git");
-  const clone = spawnSync("git", ["clone", name, "--quiet"], {
-    cwd: "/tmp"
-  });
+  const clone = spawnSync(
+    "git",
+    ["clone", `https://github.com/${fullName}`, "--quiet"],
+    {
+      cwd: "/tmp"
+    }
+  );
   if (clone.stderr.toString()) {
     return false;
   } else {
