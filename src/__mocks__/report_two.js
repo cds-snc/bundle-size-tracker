e0bd42f91e415c944d79651eacf909b1cb8bc283
Max Neuvians <max@neuvians.net>
2018-12-12 12:20:38 -0500

    Create report_two.js

---
+++ src/__mocks__/report_two.js
@@ -0,0 +1,25 @@
+export const report_two = [
+  {
+    timestamp: 1544635212,
+    files: [
+      {
+        filename: "foo/foo.main.js",
+        previous: 2024,
+        size: 2024,
+        diff: 0
+      },
+      {
+        filename: "foo/foo.a1e6f.chunk.js",
+        previous: 3024,
+        size: 3024,
+        diff: 0
+      },
+      {
+        filename: "foo/foo.a1e6t.chunk.js",
+        previous: 1914,
+        size: 1914,
+        diff: 0
+      }
+    ]
+  }
+];
