dd68195fbf5d33c5935a265a5433ccbd4228dad3
Max Neuvians <max@neuvians.net>
2018-12-12 21:34:58 -0500

    Fixed cloning and checkout

--- src/lib/checkout.js
+++ src/lib/checkout.js
@@ -1,7 +1,17 @@
 const { spawnSync } = require("child_process");
 
-module.exports.checkoutRepo = async (dir, fullName, sha) => {
+export const checkoutRepo = async (dir, fullName, sha) => {
   await require("lambda-git");
+  const name = fullName.split("/")[1];
+
+  const cleanup = spawnSync("rm", ["-rf", name], {
+    cwd: dir
+  });
+  if (cleanup.stderr.toString()) {
+    console.log(cleanup.stderr.toString());
+    return false;
+  }
+
   const clone = spawnSync(
     "git",
     ["clone", `https://github.com/${fullName}`, "--quiet"],
@@ -10,6 +20,15 @@ module.exports.checkoutRepo = async (dir, fullName, sha) => {
     }
   );
   if (clone.stderr.toString()) {
+    console.log(clone.stderr.toString());
+    return false;
+  }
+
+  const checkout = spawnSync("git", ["checkout", sha, "--quiet"], {
+    cwd: `${dir}/${name}`
+  });
+  if (checkout.stderr.toString()) {
+    console.log(checkout.stderr.toString());
     return false;
   }
 };
