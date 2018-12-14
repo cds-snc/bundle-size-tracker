c317d49931f2c9360ab9d8707c05dbf6bcdab0a7
Tim Arney <tim@line37.com>
2018-12-14 10:06:26 -0500

    add report mock

---
+++ src/__mocks__/report_three.js
@@ -0,0 +1,28 @@
+export const reportThree = [
+  {
+    timestamp: 1544798303225,
+    files: [
+      {
+        filename: "bundle.js",
+        filesize: 35421
+      },
+      {
+        filename: "template/index.html",
+        filesize: 210
+      }
+    ]
+  },
+  {
+    timestamp: 1544799918982,
+    files: [
+      {
+        filename: "bundle.js",
+        filesize: 45421
+      },
+      {
+        filename: "template/index.html",
+        filesize: 410
+      }
+    ]
+  }
+];
