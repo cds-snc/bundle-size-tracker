38133cd224cc5c37d9558baa7e43b0f30f1ed5c5
Tim Arney <tim@line37.com>
2018-12-19 11:11:36 -0500

    add moment js

--- the-app/src/components/Layout.js
+++ the-app/src/components/Layout.js
@@ -1,3 +1,6 @@
+import moment from "moment";
+import React from "react"; // eslint-disable-line
+
 export const Layout = () => {
-  return <div>Hello React SSR!</div>;
+  return <div>Hello React {moment().format()}</div>;
 };
