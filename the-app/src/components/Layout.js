5679391256af6b56f0e3c5f254dfb34d49a9b23b
Tim Arney <timarney@users.noreply.github.com>
2018-12-24 11:40:01 -0500

    cleanup (#58)

--- the-app/src/components/Layout.js
+++ the-app/src/components/Layout.js
@@ -1,16 +1,8 @@
 import React from "react"; // eslint-disable-line
 import isToday from "date-fns/is_today";
-import Button from "react-bootstrap/lib/Button"; // eslint-disable-line
-import Well from "react-bootstrap/lib/Well"; // eslint-disable-line
 
 export const Layout = () => {
-  return (
-    <div>
-      Hello React {isToday(new Date()) ? "yes" : "no"}
-      <Button bsStyle="success">Success</Button>
-      <div>
-        <Well bsSize="large">Look I'm in a large well!</Well>
-      </div>
-    </div>
-  );
+
+  return <div>Hello React {isToday(new Date()) ? "yes" : "no"} </div>;
+
 };
