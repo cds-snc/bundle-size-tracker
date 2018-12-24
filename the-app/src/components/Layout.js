01dfc5167578f95415c8715185429cb06ea894ad
Tim Arney <timarney@users.noreply.github.com>
2018-12-24 11:31:15 -0500

    test-commit (#57)

--- the-app/src/components/Layout.js
+++ the-app/src/components/Layout.js
@@ -10,7 +10,6 @@ export const Layout = () => {
       <Button bsStyle="success">Success</Button>
       <div>
         <Well bsSize="large">Look I'm in a large well!</Well>
-        <Well bsSize="small">Look I'm in a small well!</Well>
       </div>
     </div>
   );
