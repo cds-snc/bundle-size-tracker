565ea5d86105ee7b9049e83a5fc54681af4dd64f
Max <max@neuvians.net>
2018-12-27 13:21:03 -0500

    Small optimization

--- src/lib/localConfig.js
+++ src/lib/localConfig.js
@@ -1,26 +1,26 @@
 import { getFileIfExists } from "./getFile";
 import dotenv from "dotenv";
 
-const getData = async filePath => {
+const whiteList = ["BUILD_CMD", "SRC_PATH"];
+
+const getEnvData = async filePath => {
   const configData = await getFileIfExists(filePath);
 
-  if (!configData) {
-    return false;
-  }
+  if (!configData) return false;
 
   return dotenv.parse(configData);
 };
 
 export const loadLocalConfig = async filePath => {
-  const configData = await getData(filePath);
-  if (configData) {
-    const whiteList = ["BUILD_CMD", "SRC_PATH"];
-    for (let k in configData) {
-      if (whiteList.includes(k)) {
-        process.env[k] = configData[k];
-      }
+  const configData = await getEnvData(filePath);
+
+  if (!configData) return false;
+
+  Object.keys(configData).forEach(k => {
+    if (whiteList.includes(k)) {
+      process.env[k] = configData[k];
     }
-    return true;
-  }
-  return false;
+  });
+
+  return true;
 };
