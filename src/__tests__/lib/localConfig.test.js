91510fbf72abc4fa3228895096f1e7dbcf160eb1
Max <max@neuvians.net>
2018-12-27 11:36:23 -0500

    Added localConfig functins

---
+++ src/__tests__/lib/localConfig.test.js
@@ -0,0 +1,26 @@
+import { loadLocalConfig } from "../../lib/localConfig";
+import path from "path";
+
+const filePath = (filename = ".bundle-size-tracker-config") => {
+  return path.resolve(__dirname, `../../__mocks__/${filename}`);
+};
+
+describe("loads local ENV config variables specific to that repo", () => {
+  it("returns false when no data is passed", async () => {
+    try {
+      const results = await loadLocalConfig(filePath(""));
+      expect(results).toEqual(false);
+    } catch (e) {}
+  });
+
+  it("returns 1 when the plugin exists", async () => {
+    const results = await loadLocalConfig(filePath());
+    expect(results).toEqual(1);
+  });
+
+  it("only process white listed keys", async () => {
+    await loadLocalConfig(filePath());
+    expect(process.env.BUILD_CMD).toEqual("buildz");
+    expect(process.env.hasOwnProperty("FOO")).toEqual(false);
+  });
+});
