098a90c106c6969e448af2e6179146ac530f7581
Max <max@neuvians.net>
2018-12-27 11:36:23 -0500

    Added localConfig functins

---
+++ src/lib/localConfig.js
@@ -0,0 +1,29 @@
+import { getFile } from "./getFile";
+import dotenv from "dotenv";
+
+const getData = async filePath => {
+  const configData = await getFile(filePath);
+
+  if (!configData) {
+    return false;
+  }
+
+  return dotenv.parse(configData);
+};
+
+export const loadLocalConfig = async filePath => {
+  const configData = await getData(filePath);
+  if (configData) {
+    const whiteList = ["BUILD_CMD", "SRC_PATH"];
+    let count = 0;
+    for (let k in configData) {
+      if (whiteList.includes(k)) {
+        process.env[k] = configData[k];
+        count++;
+      }
+    }
+    return count;
+  } else {
+    return false;
+  }
+};
