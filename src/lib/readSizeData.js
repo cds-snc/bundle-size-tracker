ce2782c2a5b33a848503269b0c7c0197b202c8e6
Tim Arney <tim@line37.com>
2018-12-28 10:44:29 -0500

    merge

--- src/lib/readSizeData.js
+++ src/lib/readSizeData.js
@@ -2,6 +2,8 @@ import { buildSizesPath } from "./buildSizesPath";
 import { getFile } from "./getFile";
 import { cleanup } from "./checkout";
 
+const tmpPath = process.env.TMP_PATH || "/tmp";
+
 export const readFileSizeData = async name => {
   const packageData = await getFile(buildSizesPath(name));
   const result = JSON.parse(packageData);
